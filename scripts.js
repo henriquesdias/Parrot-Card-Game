20398a1910e6c36633a3e4e308ed0c1cd5d0881b
henriquesdias <henrique.dias1915@gmail.com>
2022-06-14 01:26:39 -0300

    Number os cards based on prompt

--- scripts.js
+++ scripts.js
@@ -1,14 +1,26 @@
-let firstCards = document.querySelectorAll('.firstOption');
-let secondCards = document.querySelectorAll('.secondOption');
-let frontCards = document.querySelectorAll('.frontCard');
-console.log(firstCards , secondCards);
-// let numberOfCards = prompt("Digite a quantidade de cartas que deseja jogar");
+let allCards = document.querySelectorAll('.card');
+let deck = document.querySelectorAll('.frontCard');
+let numberOfCards = prompt("Digite a quantidade de cartas que deseja jogar");
 verifyConditionForPlay();
 function verifyConditionForPlay() {
+    numberOfCards = Number(numberOfCards);
     while (numberOfCards < 4 || numberOfCards > 14 || numberOfCards % 2 !== 0) {
         numberOfCards = prompt("Digite a quantidade de cartas que deseja jogar");
     }
-    for (let i = Number(numberOfCards) / 2 ; i < Number(numberOfCards) ; i++) {
-
+    
+    for (let i = 13 ; i >= numberOfCards; i--) {
+        allCards[i].classList.add('hide');
     }
+    showCards();
+}
+
+// function comparador() { 
+// 	return Math.random() - 0.5; 
+// }
+
+function showCards() {
+    for (let i = 0 ; i < numberOfCards / 2 ; i++) {
+        allCards[i].innerHTML = deck[i].innerHTML;
+    }
+    console.log('foi chamada');
 }
