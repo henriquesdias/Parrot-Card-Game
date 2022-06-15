5ee89c20c81b2e52cc0a21aecb3a0f850b33ece0
henriquesdias <henrique.dias1915@gmail.com>
2022-06-15 02:01:25 -0300

    added a new way to add cards and they are all shuffled

--- scripts.js
+++ scripts.js
@@ -1,30 +1,66 @@
-let allCards = document.querySelectorAll('.card');
-let deck = document.querySelectorAll('.options');
-let gifs= [deck[0] , deck[1] , deck[2],  deck[3],  deck[4],  deck[5], deck[6]];
+let allCards;
+let container = document.querySelector('main');
+let cardsFinal = [];
+let gifs= [
+    '<img src="./img/bobrossparrot.gif" >',
+    '<img src="./img/explodyparrot.gif" >',
+    '<img src="./img/fiestaparrot.gif" >',
+    '<img src="./img/metalparrot.gif" >',
+    '<img src="./img/revertitparrot.gif" >',
+    '<img src="./img/tripletsparrot.gif" >',
+    '<img src="./img/unicornparrot.gif" >'
+]
 let numberOfCards = prompt("Digite a quantidade de cartas que deseja jogar");
+
 verifyConditionForPlay();
 function verifyConditionForPlay() {
     numberOfCards = Number(numberOfCards);
     while (numberOfCards < 4 || numberOfCards > 14 || numberOfCards % 2 !== 0) {
         numberOfCards = prompt("Digite a quantidade de cartas que deseja jogar");
     }
-    
-    for (let i = 13 ; i >= numberOfCards; i--) {
-        allCards[i].classList.add('hide');
+    gifs = gifs.sort(comparador);
+    for (let i = 0; i < numberOfCards / 2; i++) {
+        container.innerHTML += `
+        <div class="card" onclick="selectCard(this)">
+        <div class="backCard face">
+                <img src="./img/front.png">
+        </div>
+        <div class="frontCard face">
+            ${gifs[i]}
+        </div>
+        </div>
+        <div class="card" onclick="selectCard(this)">
+        <div class="backCard face">
+                <img src="./img/front.png">
+        </div>
+        <div class="frontCard face">
+            ${gifs[i]}
+        </div>
+        </div>`
     }
+    allCards = document.querySelectorAll('.card');
     shuffleDeck();
+
+    // let id = setInterval( showBeforeTheBeginGame , 2000);
 }
 function shuffleDeck() {
-    gifs.sort(comparador);
-    for (let i = 0 ; i < numberOfCards ; i++) {
-        allCards[i].querySelector('.frontCard').innerHTML = gifs[i].innerHTML;
+    for (let i = 0 ; i < allCards.length ; i++) {
+        cardsFinal.push(allCards[i].innerHTML);
+        cardsFinal = cardsFinal.sort(comparador);
+    }
+    for (let j = 0 ; j < allCards.length ; j++) {
+        allCards[j].innerHTML = cardsFinal[j];
     }
 }
-
 function comparador() { 
 	return Math.random() - 0.5; 
 }
-
+function showBeforeTheBeginGame() {
+    for(let i = 0 ; i < allCards.length ; i++) {
+        allCards[i].querySelector('.backCard').classList.toggle('effectBackCard');
+        allCards[i].querySelector('.frontCard').classList.toggle('effectFrontCard');
+    }
+}
 function selectCard(e) {
     e.querySelector('.backCard').classList.toggle('effectBackCard');
     e.querySelector('.frontCard').classList.toggle('effectFrontCard');
